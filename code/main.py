3bd0dd5cb646f1d4cb877766bf45d463106c9508
Richard Hoyt <richardhoyt@Richards-Air.lan>
2023-04-27 13:45:06 -0400

    Add Player class

--- code/main.py
+++ code/main.py
@@ -1,12 +1,15 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('BunBun Hollow')
         self.clock = pygame.time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
@@ -15,8 +18,9 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-                dt = self.clock.tick() / 1000
-                pygame.display.update()
+            dt = self.clock.tick() / 1000
+            self.level.run(dt)
+            pygame.display.update()
 
 
 if __name__ == '__main__':
